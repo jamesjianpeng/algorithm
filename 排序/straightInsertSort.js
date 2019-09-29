5c3f10720c76d998c5c92755bb8bfc7fa6189588
jamesjianpeng <jamesjianpeng@gmail.com>
2019-09-29 14:38:20 +0800

    feat: 插入排序和二分查找法 * 插入排序算法，JavaScript 描述 * 二分查找法，JavaScript 描述

---
+++ 排序/straightInsertSort.js
@@ -0,0 +1,33 @@
+/**
+ * @description 随机获取自定范围的数字
+ * @param {number} num 
+ * @param {number} rangMax 
+ * @param {number} rangMin 
+ */
+function getRandomNum(num, rangMax = 1000, rangMin = 100) {
+    let list =[];
+    for(var i = 0; i < num; i++) {
+        list[i] = Math.round(Math.random() * (rangMax - rangMin)) + rangMin
+    }
+    return list;
+}
+
+/**
+ * @description 插入排序，升序 
+ * @param {Array} data
+ */ 
+function straightInsertSort(data) {
+    let len = data.length;
+    let template = null
+    for (let i = 1;i < len;i++) {
+        let j = i - 1;
+        template = data[i]  /* 需要排序的值 */
+        // 从后向前比较
+        while(j - 1 >= -1 && data[j] /* 在拍好序的值 */ > template){
+            data[j+1] = data[j]; // 先交换值 
+            j--; // 向后移动
+        }
+        data[j+1] = template;
+    }
+    return data;
+}
